243f9af4cd2cbcd8ccfd3caaa9e8c01745009456
favorable-device-5158 <blee2125@gmail.com>
2020-01-14 16:41:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -57,6 +57,7 @@ class MusicLibraryController
     input= gets.chomp
     if artist= Artist.find_by_name(input)
       Artist.all.sort{|a,b| a.name<=>b.name}.each.with_index(1) do |s,i|
+        puts "#{i}. #{s.name} - #{s.genre.name}"
     end
   end
 
