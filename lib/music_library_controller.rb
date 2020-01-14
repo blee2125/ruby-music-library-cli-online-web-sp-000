795e534aa064308d8498b243a244df92c2a9aa61
favorable-device-5158 <blee2125@gmail.com>
2020-01-14 16:58:57 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -75,7 +75,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input= gets.chomp.to_i
-    if input <= Song.all.length
+    if input <= 1..Song.all.length
       puts "Playing #{song.name} by #{song.artist.name}"
 
     end
