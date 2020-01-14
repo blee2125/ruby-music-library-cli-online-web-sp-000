63ae0b9dccd13a5bd7a5cb62f3b1a541961c3ccb
favorable-device-5158 <blee2125@gmail.com>
2020-01-14 16:30:59 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,7 +47,9 @@ class MusicLibraryController
   end
 
   def list_genres
-
+    Genre.all.sort{|a,b| a.name<=>b.name}.each.with_index(1) do |s,i|
+      puts "#{i}. #{s.name}"
+    end
   end
 
   def list_songs_by_artist
