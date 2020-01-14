aff4b62ddede8657f694df830921a1f8f488c05a
favorable-device-5158 <blee2125@gmail.com>
2020-01-14 16:23:10 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -36,7 +36,7 @@ class MusicLibraryController
 
   def list_songs
     Song.all.sort{|a,b| a.name<=>b.name}.each.with_index(1) do |s,i|
-      puts "#{i}. #{s.name}"
+      puts "#{i}. #{s.artist} - #{s.name} - #{s.genre}"
     end
   end
 
